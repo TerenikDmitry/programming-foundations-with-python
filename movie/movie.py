46d7800a2366693bcab5fa3ff6466dd4729971d2
TerenikDmitry <dm.terenik@gmail.com>
2017-09-25 21:06:06 +0300

    ADDED __init__ to class Movie

---
+++ movie/movie.py
@@ -0,0 +1,6 @@
+class Movie():
+	def __init__(self, movie_title, movie_storyline, movie_image_url, movie_video_url):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.image_url = movie_image_url
+		self.video_url = movie_video_url
