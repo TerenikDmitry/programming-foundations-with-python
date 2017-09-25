09804fe56ef48056d96b6f7daad8d9fcf3a558b9
TerenikDmitry <dm.terenik@gmail.com>
2017-09-25 21:06:34 +0300

    ADDED server file

---
+++ movie/server.py
@@ -0,0 +1,5 @@
+import movie
+
+movie1 = movie.Movie("Горе-творец", "Сюжет основывается на реальной истории создания фильма «Комната», а также биографии его создателя — Томми Вайсо, которая была освещена в одноимённой книге.", "https://upload.wikimedia.org/wikipedia/ru/thumb/a/a6/The_Disaster_Artist.jpg/405px-The_Disaster_Artist.jpg", "https://www.youtube.com/watch?v=Mo2472JRhqw")
+
+print(movie1.storyline)
