4a3504a085f275276c5deaaa9f44ed62f798e8b3
TerenikDmitry <dm.terenik@gmail.com>
2017-09-26 16:08:01 +0300

    ADDED show_trailer function

--- movie/movie.py
+++ movie/movie.py
@@ -1,6 +1,12 @@
+import webbrowser
+
 class Movie():
 	def __init__(self, movie_title, movie_storyline, movie_image_url, movie_video_url):
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.image_url = movie_image_url
 		self.video_url = movie_video_url
+
+	def show_trailer(self):
+		webbrowser.open(self.video_url)
+		
